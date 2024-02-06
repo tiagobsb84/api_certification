cccda2f6612cc5e3056d4192f11ae4e815f6dda5
Tiago Silva <tiagobsb84@gmail.com>
2024-02-06 13:50:57 -0300

    Adicionando tres entidades

---
+++ certification/src/main/java/br/com/tiagosilva/certification/modules/students/entity/CertificationStudentEntity.java
@@ -0,0 +1,20 @@
+package br.com.tiagosilva.certification.modules.students.entity;
+
+import java.util.List;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CertificationStudentEntity {
+
+	private UUID id;
+	private UUID studentID;
+	private String technology;
+	private int grade;
+	List<AnswersCertificationsEntity> answersCerficationsEntity;
+}
