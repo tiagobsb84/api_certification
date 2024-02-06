347ad369d2bc6bfe449d43b40e390017e7314e72
Tiago Silva <tiagobsb84@gmail.com>
2024-02-06 09:32:03 -0300

    Iniciando projeto

---
+++ certification/src/test/java/br/com/tiagosilva/certification/CertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagosilva.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
