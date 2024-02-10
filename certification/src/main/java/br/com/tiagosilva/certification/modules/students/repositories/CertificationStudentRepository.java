0329ee374d740a62feedef9a07745b293063e28e
Tiago Silva <tiagobsb84@gmail.com>
2024-02-10 12:01:13 -0300

    Adicionando a funcionalidade de buscar pelo email e tecnologia

---
+++ certification/src/main/java/br/com/tiagosilva/certification/modules/students/repositories/CertificationStudentRepository.java
@@ -0,0 +1,15 @@
+package br.com.tiagosilva.certification.modules.students.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.tiagosilva.certification.modules.students.entity.CertificationStudentEntity;
+
+public interface CertificationStudentRepository extends JpaRepository<CertificationStudentEntity, UUID> {
+
+	 @Query("SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
+	 List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
+}
--- certification/src/main/java/br/com/tiagosilva/certification/modules/students/useCases/VerifyIsHasCertificationUseCase.java
+++ certification/src/main/java/br/com/tiagosilva/certification/modules/students/useCases/VerifyIsHasCertificationUseCase.java
@@ -1,17 +1,22 @@
 package br.com.tiagosilva.certification.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.tiagosilva.certification.modules.students.dto.VerifyHasCertificationDTO;
+import br.com.tiagosilva.certification.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIsHasCertificationUseCase {
 
+	@Autowired
+	private CertificationStudentRepository certifificationStudentRepository;
+	
 	public boolean execute(VerifyHasCertificationDTO dto) {
-		if(dto.getEmail().equals("tiago@gmail.com") && dto.getTechnology().equals("JAVA") ) {
+		var result = this.certifificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+		if(!result.isEmpty() ) {
 			return true;
 		}
-		
 		return false;
 	}
 }
