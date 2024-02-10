3c0215cc4924804b25925ceb0097333a5a66ba4b
Tiago Silva <tiagobsb84@gmail.com>
2024-02-10 11:47:16 -0300

    Adicionando tres entidades e ajustando gitignore

--- certification/src/main/java/br/com/tiagosilva/certification/modules/students/entity/StudentEntity.java
+++ certification/src/main/java/br/com/tiagosilva/certification/modules/students/entity/StudentEntity.java
@@ -3,6 +3,12 @@ package br.com.tiagosilva.certification.modules.students.entity;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +16,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
+	
+	@Column(unique = true, nullable = false)
 	private String email;
+	
+	@OneToMany(mappedBy = "studentEntity")
 	private List<CertificationStudentEntity> certificationStudentEntity;
 }
